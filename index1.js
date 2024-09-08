08dfe7460071dfa2f4bb9d97c50eec2c6dcbc8bf
saif1803 <saif48763@gmail.com>
2024-09-08 21:39:54 +0530

    destination design done

--- index1.js
+++ index1.js
@@ -1,3 +1,13 @@
+
+function hamToggle() {
+    let menu = document.querySelector('.hammenu');
+    let icon = document.getElementsByTagName('i')[0];
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-arrow-right');
+    menu.classList.toggle('hidden');
+    
+}
+
 let destinations = [];
 
 // Fetch the JSON data
