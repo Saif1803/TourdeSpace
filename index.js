d475ee0b849879bd82b72bd468c5fcde45e5145b
saif1803 <saif48763@gmail.com>
2024-09-08 19:14:52 +0530

    main page done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 function hamToggle() {
     let menu = document.querySelector('.hammenu');
+    let icon = document.getElementsByTagName('i')[0];
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-arrow-right');
     menu.classList.toggle('hidden');
     
 }
